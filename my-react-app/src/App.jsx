9d846030ba6910196e212c83197c022970b8ac6b
Roopa-S-Biradar <roopa@magnovaventures.com>
2026-05-05 16:05:40 +0530

    React practice files

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "./Card";
+import Card from "./Card";
+function App() {
+  return (
+    <>
+      <h1>Hello World !</h1>
+      <Card>
+        <h1>This is from app component</h1>
+        <p>This is from app component</p>
+      </Card>
+    </>
+  );
+}
+
+export default App;
