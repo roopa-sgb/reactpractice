2df6764a52796c2867aa51e735c0fde01ab1d3be
Roopa-S-Biradar <roopa@magnovaventures.com>
2026-05-13 18:38:54 +0530

    useState hook, textarea

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
+//import "./App.css";
+import { useState } from "react";
 import "./Card";
 import Card from "./Card";
 function App() {
+  const shoot = (a) => alert(a);
+  const cars = ["ford", "nexon", "tesla"];
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(name);
+  }
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+  }
+
   return (
     <>
       <h1>Hello World !</h1>
@@ -9,6 +26,22 @@ function App() {
         <h1>This is from app component</h1>
         <p>This is from app component</p>
       </Card>
+      <button onClick={() => shoot("Great shot !")}>Take a Shot !</button>
+      <ol>
+        {cars.map((car, i) => (
+          <li key={i}>{car}</li>
+        ))}
+      </ol>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name : </label>
+        <input type="text" id="name" value={name} onChange={handleChange} />
+        <input type="submit" />
+        <label>
+          Write here :
+          <textarea value={text} onChange={handleTextChange} />
+        </label>
+      </form>
+      <p>{text}</p>
     </>
   );
 }
